475af8ce7af296f1534906cf2083d7577818e4a7
unknown <antonjailert@gmail.com>
2022-09-02 11:42:05 +0300

    Small corrections

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -3,11 +3,11 @@
 Console.WriteLine("Enter number: ");
 int N = int.Parse(Console.ReadLine()?? "0");
 int sum = 0;
-    
-    while (N > 0)
+   
+    for (int i = 0; N > 0; i++)
     {
         sum = sum + N % 10;
-        N = N /10 ;
+        N = N /10 ;   
     }
 
  Console.WriteLine($"Digit sum = {sum}");
